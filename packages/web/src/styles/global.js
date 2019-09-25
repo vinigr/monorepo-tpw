8baf7e2d468d6c9651cc7172d35b8fb062a40f0d
Vinícios Grama <vinyirun1@gmail.com>
2019-09-25 17:27:45 -0300

    :lipstick: Cria arquivo de estilos global

---
+++ packages/web/src/styles/global.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+  }
+
+  html, body, #root {
+    min-height: 100vh;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased !important;
+  }
+`;
