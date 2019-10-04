0e54201fe433330cf69a3d145eaf64947a4fbef1
Vinícios Grama <vinyirun1@gmail.com>
2019-10-04 15:20:08 -0300

    :construction: Cria função para buscar artigos

--- packages/web/src/pages/Search/Search.jsx
+++ packages/web/src/pages/Search/Search.jsx
@@ -7,14 +7,42 @@ import ArticleItem from '../../components/ArticleItem/ArticleItem.jsx';
 
 import logo from '../../assets/images/rede-ftc.png';
 
+import api from '../../service/api';
+
 export default function Search(props) {
   const [text, setText] = useState('');
+  const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const { article } = queryString.parse(props.location.search);
     setText(article);
+
+    if (!article) {
+      return;
+    }
+
+    async function fetchData() {
+      try {
+        const { data } = await api.post('/trabalho', {
+          pesquisa: article,
+        });
+        setArticles(data.trabalhos);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchData();
   }, [props.location.search]);
+
+  function keyPress(e) {
+    if (e.keyCode === 13) {
+      props.history.push(`/search?article=${text}`);
+    }
+  }
+
   return (
     <Container>
       <section id="area-search">
@@ -26,13 +54,12 @@ export default function Search(props) {
           value={text}
           onChange={e => setText(e.target.value)}
           placeholder="Pesquisar"
+          onKeyDown={keyPress}
         />
       </section>
       <section id="results">
         {!loading ? (
-          <ul>
-            <ArticleItem />
-          </ul>
+          <ul>{articles && articles.map(a => <ArticleItem article={a} />)}</ul>
         ) : (
           <span>Carregando...</span>
         )}
