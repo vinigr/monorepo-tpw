4aef5f25f5238db20b94357f857e883989b69f2a
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2019-10-04 01:10:29 -0300

    Add search by title, keywords or abstract

--- packages/server/src/app/controllers/TrabalhoController.js
+++ packages/server/src/app/controllers/TrabalhoController.js
@@ -1,10 +1,25 @@
+const yup = require('yup');
+
 const Trabalho = require('../models/Trabalho');
 
 class TrabalhoController {
-    async index(req, res) {
-        const retorno = await Trabalho.find();
-        return res.json(retorno);
+  async index(req, res) {
+    const schema = yup.object().shape({
+      pesquisa: yup.string().required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Entrada inválida' });
     }
+
+    const { pesquisa } = req.body;
+    const trabalhos = await Trabalho.find({ $or:[
+      {'titulo': new RegExp(pesquisa, 'i')},
+      {'palavrasChave': new RegExp(pesquisa, 'i')},
+      {'resumo': new RegExp(pesquisa, 'i')},
+    ]});
+    return res.json(trabalhos);
+  }
 }
 
 module.exports = new TrabalhoController();
