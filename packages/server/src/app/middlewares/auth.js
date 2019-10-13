bce20b5c4eef1cf7215af13c4c5882941a01bff6
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2019-10-12 22:31:25 -0300

    Adicionando middleware de autenticação

---
+++ packages/server/src/app/middlewares/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+
+const authConfig = require('../../config/auth');
+
+module.exports = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token não fornecido' });
+  }
+
+  const [, token] = authHeader.split(' ');
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    req.idUsuario = decoded.id;
+    req.professor = decoded.professor;
+    req.administrador = decoded.administrador;
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token inválido' });
+  }
+};
