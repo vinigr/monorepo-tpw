6cddbfd2cc48393894077eb084e680a062763fe6
Vinícios Grama <vinyirun1@gmail.com>
2019-10-17 00:23:15 -0300

    :speech_balloon: Cria arquivo para simulação de requisição

---
+++ packages/web/src/service/data.js
@@ -0,0 +1,6 @@
+export const users = [
+  { id: 1, email: 'vinicios@teste.com', nome: 'Vinícios G.' },
+  { id: 2, email: 'daniel@teste.com', nome: 'Daniel C.' },
+  { id: 3, email: 'marllus@teste.com', nome: 'Marllus S.' },
+  { id: 4, email: 'stefany@teste.com', nome: 'Stefany A.' },
+];
