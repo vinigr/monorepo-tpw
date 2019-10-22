6308e59f3975d77e04560952747fc993840b629e
Vinícios Grama <vinyirun1@gmail.com>
2019-10-22 12:37:30 -0300

    :sparkles: Cria botão de acesso a tela de criação de artigos

--- packages/web/src/components/MenuAvatar/MenuAvatar.jsx
+++ packages/web/src/components/MenuAvatar/MenuAvatar.jsx
@@ -23,6 +23,11 @@ export default function MenuAvatar(props) {
       <LinkMenu to="/account">
         <MenuItem onClick={handleClose}>Minha conta</MenuItem>
       </LinkMenu>
+      {AuthService.loggedIn() && AuthService.getRole() !== 'aluno' && (
+        <LinkMenu to="/send">
+          <MenuItem onClick={handleClose}>Criar artigo</MenuItem>
+        </LinkMenu>
+      )}
       <MenuItem onClick={logout}>Sair</MenuItem>
     </Menu>
   );
