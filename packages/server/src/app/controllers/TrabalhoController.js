9b7082347aa7eefc39ea102f703e50e56e210706
Vinícios Grama <vinyirun1@gmail.com>
2019-10-24 12:37:13 -0300

    :construction: Inicia criação de função para atualizar informações do artigo

--- packages/server/src/app/controllers/TrabalhoController.js
+++ packages/server/src/app/controllers/TrabalhoController.js
@@ -26,23 +26,59 @@ class TrabalhoController {
 
   async store(req, res) {
     const schema = yup.object().shape({
-      authors: yup.string().required(),
+      authors: yup.object().required(),
       othersAuthors: yup.string(),
     });
 
-    if (!(await schema.isValid(req.params))) {
+    if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Entrada inválida' });
     }
 
-    const { authors, othersAuthors } = req.body;
+    const { authors, othersAuthors, advisor } = req.body;
 
     try {
       await Trabalho.create({
         autores: authors,
         outrosAutores: othersAuthors,
+        orientador: advisor,
+        professor: req.idUsuario,
         editavel: true,
         publicado: false,
       });
+      return res.send();
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  }
+
+  async update(req, res) {
+    const { title, summary, keywords } = req.body;
+    const { id } = req.params;
+
+    try {
+      const article = await Trabalho.findById({ _id: id });
+
+      if (!article) {
+        return res.status(400).send({ message: 'Artigo não encontrado!' });
+      }
+
+      if (
+        article.autores.indexOf(req.idUsuario) === -1 &&
+        article.orientador !== req.idUsuario &&
+        article.professor !== req.idUsuario
+      ) {
+        return res.status(400).send({ message: 'Usuário não autorizado!' });
+      }
+
+      await Trabalho.updateOne(
+        { _id: id },
+        {
+          titulo: title,
+          resumo: summary,
+          palavrasChave: keywords,
+        }
+      );
+
       return res.send();
     } catch (error) {
       return res.status(400).send(error);
@@ -65,7 +101,8 @@ class TrabalhoController {
       const articles = await Trabalho.find({})
         .sort({ created_at: -1 })
         .limit(5)
-        .populate('autores', 'nome');
+        .populate('autores', 'nome')
+        .populate('orientador', 'nome');
       return res.json(articles);
     } catch (err) {
       return res.send(400).json(err);
@@ -86,42 +123,51 @@ class TrabalhoController {
     }
   }
 
-  async switchEditable(_, res){
+  async switchEditable(_, res) {
     const { id } = req.params;
 
     const article = await Trabalho.findOne({ _id: id });
 
-    if(!article){
-      return res.status(400).json({ error: "This article does not exists" });
+    if (!article) {
+      return res.status(400).json({ error: 'This article does not exists' });
     }
 
-    if(article.professor != req.idUsuario) {
-      return res.status(401).json({ error: "You do not have permisson to change this article status" })
+    if (article.professor != req.idUsuario) {
+      return res.status(401).json({
+        error: 'You do not have permisson to change this article status',
+      });
     }
 
-    const articleUpdated = await Trabalho.updateOne({ _id: id }, { editavel: !article.editavel });
+    const articleUpdated = await Trabalho.updateOne(
+      { _id: id },
+      { editavel: !article.editavel }
+    );
 
     return res.json(articleUpdated);
   }
 
-  async switchPublished(_, res){
+  async switchPublished(_, res) {
     const { id } = req.params;
 
     const article = await Trabalho.findOne({ _id: id });
 
-    if(!article){
-      return res.status(400).json({ error: "This article does not exists" });
+    if (!article) {
+      return res.status(400).json({ error: 'This article does not exists' });
     }
 
-    if(article.professor != req.idUsuario) {
-      return res.status(401).json({ error: "You do not have permisson to change this article status" })
+    if (article.professor != req.idUsuario) {
+      return res.status(401).json({
+        error: 'You do not have permisson to change this article status',
+      });
     }
 
-    const articleUpdated = await Trabalho.updateOne({ _id: id }, { publicado: !article.publicado });
+    const articleUpdated = await Trabalho.updateOne(
+      { _id: id },
+      { publicado: !article.publicado }
+    );
 
     return res.json(articleUpdated);
   }
-
 }
 
 module.exports = new TrabalhoController();
