b7a606709e4c480fe37affa1c15c113e9d44b8ee
Vinícios Grama <vinyirun1@gmail.com>
2019-10-24 10:17:35 -0300

    :bug: Corrige verificador de função do usuário

--- packages/server/src/app/middlewares/verifyJwt.js
+++ packages/server/src/app/middlewares/verifyJwt.js
@@ -1,6 +1,6 @@
 module.exports = {
   isTeacher(req, res, next) {
-    if (!req.professor) {
+    if (!req.professor && !req.administrador) {
       return res.status(403).send({ message: 'Require Teacher Role!' });
     }
 
