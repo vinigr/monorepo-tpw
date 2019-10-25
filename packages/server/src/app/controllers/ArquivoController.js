015e3c6b9573eccae0e7e2b6a9c306f9d600ab64
Vinícios Grama <vinyirun1@gmail.com>
2019-10-24 23:52:00 -0300

    :bug: Corrige busca síncrona no banco

--- packages/server/src/app/controllers/ArquivoController.js
+++ packages/server/src/app/controllers/ArquivoController.js
@@ -1,6 +1,3 @@
-const path = require('path');
-const fs = require('fs');
-
 const Trabalho = require('../models/Trabalho');
 
 class ArquivoController {
@@ -9,7 +6,7 @@ class ArquivoController {
     const { idUsuario } = req;
     const { filename } = req.file;
 
-    const trabalho = Trabalho.findById(idTrabalho);
+    const trabalho = await Trabalho.findById(idTrabalho);
 
     if (trabalho.autores.indexOf(idUsuario) === -1) {
       return res.status(401).json({
